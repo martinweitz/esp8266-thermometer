9759d24ffb142b429938b20f22d804a347707007
Martin Weitz <martin@weitz-online.eu>
2021-03-30 13:58:23 +0200

    Rotation changed

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,12 @@
 #define TFT_DC         D8 // define Pin D8 as DC
 
 // create instance of Adafruit_ST7735 class
-
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+#define ROTATION_NO 0
+#define ROTATION_90 1
+#define ROTATION_180 2
+#define ROTATION_270 3
 
 // forward declarations of functions
 void tftPrintTest();
@@ -21,11 +25,11 @@ void setup() {
  
  // Use this initializer if using a 1.8" TFT screen:
 tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
+tft.setRotation(ROTATION_270);
 
 tftDrawFrame();
 tftDrawLabels();
 delay(1000);
-
 }
 
 void loop() {
@@ -39,17 +43,19 @@ void tftDrawFrame()
 tft.fillScreen(ST77XX_BLACK);
 tft.drawRoundRect(1, 0, tft.width()-1, 30, 10, ST77XX_BLUE);
 tft.drawRoundRect(1, 35, tft.width()-1, 90, 10, ST77XX_GREEN);
-tft.drawRoundRect(1, 130, tft.width()-1, 30, 10, ST77XX_RED);
 }
 
 void tftDrawLabels()
 {
 tft.setTextColor(ST77XX_WHITE);
+tft.setCursor(15,8);
+tft.setTextSize(2);
+tft.print("Thermometer");
 tft.setCursor(10,40);
 tft.setTextSize(1);
 tft.print("Temperatur in ");
 tft.setTextSize(0);
 tft.print("O");
-tft.setTextSize(1);
+tft.setTextSize(2);
 tft.print("C");
 }
